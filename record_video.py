a2e6aac26d60644c86378bbf75ead53acd2d0cad
Eugene Moon <uoojin95@gmail.com>
2019-07-22 21:23:53 -0400

    finished the video recording module

--- record_video.py
+++ record_video.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
-
-import sys
 import cv2
+import sys
+import numpy as np
 
-def recordVideo(output_file_name):
+PATH_TO_DATA = '../data/'
+
+def recordVideo(filename):
   # Create a VideoCapture object
   cap = cv2.VideoCapture(0)
   
@@ -11,23 +12,25 @@ def recordVideo(output_file_name):
   if (cap.isOpened() == False): 
     print("Unable to read camera feed")
   
-  # use resolution of the captured image
+  # Default resolutions of the frame are obtained.
   frame_width = int(cap.get(3))
   frame_height = int(cap.get(4))
   
   # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-  out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+  out = cv2.VideoWriter(PATH_TO_DATA + filename, cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
   
   while(True):
     ret, frame = cap.read()
-  
-    if ret == True: 
-      # gray out the video
-      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      out.write(gray)
+
+    # grayout
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if ret == True:
+      # Write the frame into the file 'output.avi'
+      out.write(frame)
   
       # Display the resulting frame    
-      cv2.imshow('frame',frame)
+      cv2.imshow('frame', frame)
   
       # Press Q on keyboard to stop recording
       if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -42,9 +45,9 @@ def recordVideo(output_file_name):
   out.release()
   
   # Closes all the frames
-  cv2.destroyAllWindows()
+  cv2.destroyAllWindows() 
 
 if __name__ == "__main__":
-  args = sys.argv[1:]
-  output_file_name = args[0]
-  recordVideo(output_file_name)
+  args = sys.argv
+  print(args)
+  recordVideo(args[1])
