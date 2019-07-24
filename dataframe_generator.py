b81d06f3036c298de037be3f2fd41c93c208a4c8
Eugene Moon <uoojin95@gmail.com>
2019-07-23 22:37:55 -0400

    finished csv writer

--- dataframe_generator.py
+++ dataframe_generator.py
@@ -1,7 +1,10 @@
 import os
+import sys
 import numpy as np
+import pandas as pd
 
 PATH_TO_IMG_FOLDERS = '../data/imgs/'
+PATH_TO_DATAFRAME_FOLDER = '../data/dataframes/'
 FILE_TO_IGNORE = {'.DS_Store'}
 
 def sortByImageNumber(x):
@@ -11,7 +14,9 @@ def filterIgnored(path):
   z = [x for x in os.listdir(path) if x not in FILE_TO_IGNORE]
   return z
 
-def dataframeGenerator():
+def dataframeGenerator(filename):
+  filename_to_class = []
+
   img_folders = filterIgnored(PATH_TO_IMG_FOLDERS)
 
   for folder in sorted(img_folders):
@@ -19,7 +24,22 @@ def dataframeGenerator():
     imgs = filterIgnored(PATH_TO_IMG_FOLDERS + image_class)
 
     for img in sorted(imgs, key=sortByImageNumber):
-      print('image!', img)
+      gesture_class = img.split('.')[0].split('-')[0]
+      filename_to_class.append([img, gesture_class])
+  
+  # create pandas dataframe that will be exported as a csv file
+  img_data_df = pd.DataFrame(columns=['filename', 'class'], data=filename_to_class)
+
+  # wrtie to a csv file
+  img_data_df.to_csv(PATH_TO_DATAFRAME_FOLDER + filename, encoding='utf-8', index=False)
+  print('created csv file named {filename}'.format(filename=filename))      
 
 if __name__ == "__main__":
-  dataframeGenerator()
+  args = sys.argv
+
+  if len(args) > 1:
+    filename = args[1]
+  else:
+    filename = 'filename_to_class.csv'
+
+  dataframeGenerator(filename)
